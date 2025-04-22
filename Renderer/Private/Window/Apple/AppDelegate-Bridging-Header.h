7051fb359d69dd710f6614aed6641da38096e904
Filip Lundgren <filip@sceneri.com>
2025-04-21 17:32:12 -0700

    Fix visionOS build

--- Renderer/Private/Window/Apple/AppDelegate-Bridging-Header.h
+++ Renderer/Private/Window/Apple/AppDelegate-Bridging-Header.h
@@ -1,3 +1,8 @@
+#include <Common/Platform/ConfigureCompiler.h>
+#include <Common/Platform/ConfigureArchitecture.h>
+#include <Common/Platform/ConfigurePlatform.h>
+#include <Common/Platform/ConfigureVectorization.h>
+
 #if PLATFORM_APPLE_IOS || PLATFORM_APPLE_VISIONOS
 #import "../../../Renderer/Public/Renderer/Window/iOS/AppDelegate.h"
 #import "../../../Renderer/Public/Renderer/Window/iOS/ViewController.h"
